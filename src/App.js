fdf3d78af7f4950848c30c8ed4591ce87186dbd0
unknown <jattinjaggi2002@gmail.com>
2024-05-18 17:58:29 +0530

    commit 1.1

--- src/App.js
+++ src/App.js
@@ -13,22 +13,30 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            rotateAngle: 0
+            rotateAngle: 0,
+            cursorOnScreen: false
         };
     }
 
     handleEyeRotation = (event) => {
         const eye = document.getElementById('rotatingeye');
-        const rectEye = eye.getBoundingClientRect();
-        const centerX = rectEye.left + rectEye.width / 2;
-        const centerY = rectEye.top + rectEye.height / 2;
-        const angle = Math.atan2(event.clientY - centerY, event.clientX - centerX) * (180 / Math.PI);
-        this.setState({ rotateAngle: angle });
+        if (eye) {
+            const rectEye = eye.getBoundingClientRect();
+            const centerX = rectEye.left + rectEye.width / 2;
+            const centerY = rectEye.top + rectEye.height / 2;
+            const angle = Math.atan2(event.clientY - centerY, event.clientX - centerX) * (180 / Math.PI);
+            this.setState({ rotateAngle: angle });
+        }
     }
+
+    handleMouseEnter = () => {
+        this.setState({ cursorOnScreen: true });
+    }
+
 
     render() {
         return (
-            <div className='App' onMouseMove={this.handleEyeRotation}>
+            <div className='App' onMouseMove={this.handleEyeRotation} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
                 <NavBar />
                 <Me scrollToSection={this.scrollToSection} />
                 <TechStack />
@@ -36,7 +44,7 @@ class App extends Component {
                 <Projects />
                 <Hobbies />
                 <Scrollbutton />
-                <Cat rotateAngle={this.state.rotateAngle} />
+                {this.state.cursorOnScreen && <Cat rotateAngle={this.state.rotateAngle} />}
             </div>
         );
     }
